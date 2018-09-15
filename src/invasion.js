a877d4702e7a9b7c3a7f62104538e33fac6fc8a4
Guy Morita <guymorita@gmail.com>
2018-09-15 12:10:45 -0400

    Add invadeCommand

--- src/invasion.js
+++ src/invasion.js
@@ -1,5 +1,13 @@
 
 
-export const invade = () => {
-    return true
+const invade = (param1) => {
+    return 5
 }
+
+const invadeCommand = () => {
+    const args = process.argv
+    const numOfAliens = args[1]
+    return invade(numOfAliens)
+}
+
+module.exports = { invade, invadeCommand }
